d781326c8a8c5df3063d811d30905a27d2a80608
hgtlzyc <hgtlzycwk@gmail.com>
2021-06-12 19:48:28 -0400

    able to resume the tasks even after user killed the app

---
+++ PokemonDisplay/State/DataFlow/AppCommandsUtility/LoadPokemonPressor.swift
@@ -0,0 +1,70 @@
+//
+//  LoadPokemonPressor.swift
+//  PokemonDisplay
+//
+//  Created by lijia xu on 6/12/21.
+//
+
+import Foundation
+import Combine
+
+
+//Will be reconstructed for network calls in the end
+struct PokemonLoadingProcessSimulator{
+    let maxTasks: Int
+    let delayInSeconds: Double
+    
+    init?( maxTasks: Int, delay: Double) {
+        //set the back pressure Limits here
+        guard maxTasks > 0 && delay > 0 else {return nil}
+        self.maxTasks = maxTasks
+        self.delayInSeconds = delay
+    }
+    
+    func process <C: Collection>(in stateCenter: StateCenter, sourceCollection: C, reloadAll: Bool) -> Void where C.Element == Int{
+        var baseArray = [(Int,String)]()
+        if reloadAll == false, let currentBaseDic = stateCenter.appState.pokemonListState.pokemonsDic {
+            baseArray = currentBaseDic.map{($0, $1.name)}
+        }
+        
+        //simulate network calls with random arrive time
+        let stringBase = ["picapica", "dragon", "bird", "cat", "dog"]
+        let tupleBaseArray = Array(sourceCollection)
+            .map { ( $0, (stringBase.randomElement() ?? "nobase") + "   \($0)") }
+            .shuffled()
+        
+        tupleBaseArray
+            .publisher
+            //backpressure management
+            .flatMap(maxPublishers: .max(maxTasks) ) { tuple in
+                Just(tuple).delay(for: .seconds(delayInSeconds) , scheduler: DispatchQueue(label: "\(tuple.0)") )
+            }
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .scan(baseArray) { $0 + [$1] }
+            .receive(on: DispatchQueue.main)
+            .handleEvents(receiveCancel: {
+                stateCenter.executeAction(
+                    .receivedPokemons(.failure(.subscriptionCancelled), isFinished: true)
+                )
+            })
+            .eraseToAnyPublisher()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    stateCenter.executeAction(
+                        .receivedPokemons( nil ,isFinished: true)
+                    )
+                }
+            }, receiveValue: { tupleArray in
+                let viewModelDic = tupleArray.reduce(into: [Int: PokemonViewModel]()) { result, nextTuple in
+                    result[nextTuple.0] = PokemonViewModel(dataModel: PokemonDataModel(id: nextTuple.0, name: nextTuple.1))
+                }
+                
+                stateCenter.executeAction(
+                    .receivedPokemons( .success(viewModelDic) ,isFinished: false)
+                )
+            })
+            .store(in: &stateCenter.subscriptions[.loadingPokemon]!)
+    }
+
+}
