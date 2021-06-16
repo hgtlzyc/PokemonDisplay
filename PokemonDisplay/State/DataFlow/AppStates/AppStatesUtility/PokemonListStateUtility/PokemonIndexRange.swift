90a213f3e1d6712cbc4f09f085c64a4ec7e4360f
hgtlzyc <hgtlzycwk@gmail.com>
2021-06-15 20:39:14 -0400

    prepare for changing the lower index

--- PokemonDisplay/State/DataFlow/AppStates/AppStatesUtility/PokemonListStateUtility/PokemonIndexRange.swift
+++ PokemonDisplay/State/DataFlow/AppStates/AppStatesUtility/PokemonListStateUtility/PokemonIndexRange.swift
@@ -10,4 +10,14 @@ import Foundation
 struct PokemonIndexRange: Codable {
     var lowerBound: Int
     var upperInclusiveBound: Int
+    
+    init(lowerBound: Int, upperInclusiveBound: Int) {
+        if lowerBound <= upperInclusiveBound {
+            self.lowerBound = lowerBound
+            self.upperInclusiveBound = upperInclusiveBound
+        } else {
+            self.lowerBound = lowerBound
+            self.upperInclusiveBound = lowerBound
+        }
+    }
 }
